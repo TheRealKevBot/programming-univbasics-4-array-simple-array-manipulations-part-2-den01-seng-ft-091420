64e23aa1177c3ae819617e93f129116a65a9fec1
Kevin Glasgow <kglas001@gmail.com>
2020-09-05 00:09:52 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -11,11 +11,14 @@ def using_uniq(array)
 end 
 
 def using_flatten(array)
+  array.flatten
 end 
 
 def using_delete(array, string)
+  array.delete(string)
 end 
 
 def using_delete_at(array, integer)
+  array.delete_at 
 end 
 
