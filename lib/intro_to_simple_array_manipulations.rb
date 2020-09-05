b58b356551b87589c8309868e0187c82e4d2090f
Kevin Glasgow <kglas001@gmail.com>
2020-09-05 00:06:49 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,7 +2,8 @@ def using_concat(array_1, array_2)
   array_1.concat(array_2)
 end 
 
-def using_insert(array)
+def using_insert(array, element)
+  array.insert(element)
 end 
 
 def using_uniq(array)
